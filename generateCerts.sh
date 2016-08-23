454d2a7ba3db9f3f01039ae0b36b37d7814718e6
nsperrin <nsperrin@mckendree.edu>
2016-08-23 10:22:31 -0500

    s4: auto fills input for generating cert request. still no way of knowing it will work with https until I try -NP

--- generateCerts.sh
+++ generateCerts.sh
@@ -2,7 +2,17 @@
 
 if [ ! -f bin/cert.pem ]; then
     openssl genrsa -out bin/key.pem
-    openssl req -new -key bin/key.pem -out bin/csr.pem
+    openssl req -new -key bin/key.pem -out bin/csr.pem << STDIN
+US
+Illinois
+Lebanon
+HellaFire
+SeniorSeminar
+.
+hellafire.test@gmail.com
+P@$$(0d3
+HF
+STDIN
     openssl x509 -req -days 9999 -in bin/csr.pem -signkey bin/key.pem -out bin/cert.pem
     rm bin/csr.pem
 fi
