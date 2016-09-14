f9a76666bba890d000bf4ffc6c4915d0ef0b5aaa
Devon S. VanNote <dvon5150@yahoo.com>
2016-09-14 12:56:39 -0500

    started on nodejs Login System with Pass. On Part 1 at 14:33 minute mark

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 var express = require('express');
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sharedSession = require("express-socket.io-session");
-
+mongoose.connect('mongodb://localhost/glimpse');
+var db = mongoose.connection
 var routes = require('./routes/index');
 
 var app = express();
@@ -20,6 +27,42 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value) {
+        var namespace = param.split('.')
+            , root    = namespace.shift()
+            , formParam = root;
+
+        while(namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg   : msg,
+            value : value
+        };
+    }
+}));
+
+app.use(flash());
+
+app.use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (res,req, next){
+   res.locals.success_msg = req.flash('success_msg');
+   res.locals.error_msg = req.flash('error_msg');
+   res.locals.error = req.flash('error');
+   next();
+});
+
+app.use('/', routes);
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', (req, res, next)=>{
   res.render('index', { title: 'Express' });
 });
 
+router.get('/register', function(req,res){
+  res.render('register');
+});
+
+router.get('/login', function(reg,res) {
+  res.render('login');
+});
+
 module.exports = router;
