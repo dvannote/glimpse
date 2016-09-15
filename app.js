bf8be9ddace0a68dea54184280c7e3b7b918aaa2
Devon S. VanNote <dvon5150@yahoo.com>
2016-09-15 12:12:34 -0500

    On Step 2 on node JS login app. Getting 500 on favicon.ico for some reason. Express validator also not working.

--- app.js
+++ app.js
@@ -93,4 +93,6 @@ app.use(function(err, req, res, next) {
 
 
 
+
+
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,17 @@ router.get('/', (req, res, next)=>{
   res.render('index', { title: 'Express' });
 });
 
-router.get('/register', function(req,res){
-  res.render('register');
+router.post('/signup', function(req,res){
+    var un = req.body.username
+    var fn = req.body.firstName;
+    var ln = req.body.lastName;
+    var email = req.body.email;
+    var password = req.body.password;
+    var password2 = req.body.password2;
+
+    //Validations
+    req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
 });
 
 router.get('/login', function(reg,res) {
