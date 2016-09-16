13228a866e5618800d29e76ed38557f58ab7fb53
Devon S. VanNote <dvon5150@yahoo.com>
2016-09-16 16:55:03 -0500

    got registering working and showing up in the database! Need to account for duplicate entries still.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 var express = require('express');
-var flash = require('connect-flash');
+var flash = require('req-flash');
 var session = require('express-session');
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
@@ -12,39 +12,28 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sharedSession = require("express-socket.io-session");
+var expressValidator = require("express-validator");
 mongoose.connect('mongodb://localhost/glimpse');
+mongoose.Promise = global.Promise;
 var db = mongoose.connection
+
 var routes = require('./routes/index');
 
 var app = express();
 
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
-
-app.use(expressValidator({
-    errorFormatter: function(param, msg, value) {
-        var namespace = param.split('.')
-            , root    = namespace.shift()
-            , formParam = root;
-
-        while(namespace.length) {
-            formParam += '[' + namespace.shift() + ']';
-        }
-        return {
-            param : formParam,
-            msg   : msg,
-            value : value
-        };
-    }
-}));
-
-app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(session({
     secret: 'secret',
@@ -52,8 +41,7 @@ app.use(session({
     resave: true
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(flash());
 
 app.use(function (res,req, next){
    res.locals.success_msg = req.flash('success_msg');
@@ -61,8 +49,6 @@ app.use(function (res,req, next){
    res.locals.error = req.flash('error');
    next();
 });
-
-app.use('/', routes);
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', (req, res, next)=>{
   res.render('index', { title: 'Express' });
 });
 
-router.post('/signup', function(req,res){
-    var un = req.body.username
-    var fn = req.body.firstName;
-    var ln = req.body.lastName;
+router.get('/register', (req, res, next)=>{
+    res.render('index');
+});
+
+router.post('/register', function(req,res){
+    var username = req.body.username;
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
     var email = req.body.email;
     var password = req.body.password;
     var password2 = req.body.password2;
 
     //Validations
+    req.checkBody('username', 'Name is required').notEmpty();
+    req.checkBody('firstName', 'First Name is required').notEmpty();
+    req.checkBody('lastName', 'Last Name is required').notEmpty();
     req.checkBody('email', 'Email is not valid').isEmail();
+    req.checkBody('password', 'password is required').notEmpty();
     req.checkBody('password2', 'Passwords do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+
+    if (!errors) {
+        var newUser = new User({
+            username: username,
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        });
+
+        User.createUser(newUser, function (err, user) {
+            if (err) throw err;
+            console.log(user);
+        });
+
+        res.redirect('/')
+    } else {
+        res.render('/', {
+            errors: errors
+        });
+    }
 });
 
 router.get('/login', function(reg,res) {
