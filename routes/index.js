e7bc72b20cd95f57c94a41059396625ca6d906d8
Devon S. VanNote <dvon5150@yahoo.com>
2016-10-16 20:42:55 -0500

    think it's working now. Need to head to a different location to test it.

--- routes/index.js
+++ routes/index.js
@@ -23,8 +23,32 @@ router.get('/privacyPolicy', (req, res, next)=>{
 router.post('/getLocation', function(req, res){
     lat = req.body.lat;
     lon = req.body.lon;
-    console.log(lat,lon);
+    var flickr = require("flickrapi"),
+        flickrOptions = {
+            api_key: "a17c6a5fadc32351ab403086ff9dcce1",
+            secret: "72798014d0f72e4d",
+            user_id: "143989775@N06",
+            access_token: '72157673534760490-23dbf88abf3ac111',
+            access_token_secret: '9363ae4af815127f'
+        };
+
+    flickr.authenticate(flickrOptions, function(error, flickr) {
+        flickr.photos.search({
+            lat: lat,
+            lon: lon,
+            accuracy: 11,
+            radius:5,
+            page: 1,
+            per_page: 500,
+            safe_search: 2
+        }, function(err, result) {
+            if(err) throw err;
+            photo = (JSON.stringify(result.photos.photo));
+        });
+    });
 });
+
+
 
 function ensureAuthenticate(req,res,next){
     if(req.isAuthenticated()){
@@ -33,30 +57,6 @@ function ensureAuthenticate(req,res,next){
         res.render('/');
     }
 }
-
-var flickr = require("flickrapi"),
-    flickrOptions = {
-        api_key: "a17c6a5fadc32351ab403086ff9dcce1",
-        secret: "72798014d0f72e4d",
-        user_id: "143989775@N06",
-        access_token: '72157673534760490-23dbf88abf3ac111',
-        access_token_secret: '9363ae4af815127f'
-    };
-
-flickr.authenticate(flickrOptions, function(error, flickr) {
-    flickr.photos.search({
-        lat: 38.6081376,
-        lon:-89.82540069999999,
-        accuracy: 11,
-        radius:5,
-        page: 1,
-        per_page: 500,
-        safe_search: 2
-    }, function(err, result) {
-        if(err) throw err;
-        photo = (JSON.stringify(result.photos.photo));
-    });
-});
 
 router.post('/register', function(req,res){
     var username = req.body.username;
