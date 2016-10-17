6df94473fce4e52d41fe118073d41c04620b512f
Devon S. VanNote <dvon5150@yahoo.com>
2016-10-16 20:52:00 -0500

    cleaned up generateCerts

--- generateCerts.sh
+++ generateCerts.sh
@@ -1,32 +1,13 @@
 #!/bin/bash
 
-#Nicks original cert/key maker
-if [ ! -f bin/cert.pem ]; then
-    openssl genrsa -out bin/key.pem
-    openssl req -new -key bin/key.pem -out bin/csr.pem << STDIN
+[ -f bin/cert.pem ] && [ -f bin/key.pem ] || \
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout bin/key.pem -out bin/cert.pem << STDIN
 US
 Illinois
 Lebanon
-HellaFire
-SeniorSeminar
+PGRobot
+Glimpse
 .
-hellafire.test@gmail.com
-P@$$(0d3
-HF
+dvannote@outlook.com
 STDIN
-    openssl x509 -req -days 9999 -in bin/csr.pem -signkey bin/key.pem -out bin/cert.pem
-    rm bin/csr.pem
-fi
-
-##Brian's cert/key maker. use if Nick's doesn't work
-#[ -f bin/cert.pem ] && [ -f bin/key.pem ] || \
-#openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout bin/key.pem -out bin/cert.pem << STDIN
-#US
-#Illinois
-#Lebanon
-#HellaFire
-#SeniorSeminar
-#.
-#hellafire.test@gmail.com
-#STDIN
 
